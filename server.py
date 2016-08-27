8efd30765a6409d2b07da21a1a54fc8a2d278497
Saswata Gupta <gupta.saswata@gmail.com>
2016-08-27 12:00:17 -0400

    Now ignore all imgs for the repo for cleaner commits

--- server.py
+++ server.py
@@ -4,8 +4,9 @@ from imageGenerator import *
 app = Flask(__name__)
 
 @app.route('/')
-def index(go=None):       
-    return render_template("index.html", imgUrl = getRandImage())
+def index(go=None):
+    imgId  = generate()
+    return render_template("index.html", imgUrl =imgId)
 
 @app.route('/_new_art')
 def new_art():
